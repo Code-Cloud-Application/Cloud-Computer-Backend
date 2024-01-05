626e5e07f6a2e8b4ce1234b9b49838c011e4df3c
等陌上花开 <zhzhn2002@gmail.com>
2024-01-05 11:28:10 +0800

    完善了banner

--- src/main/java/cloud/computer/backend/BackendApplication.java
+++ src/main/java/cloud/computer/backend/BackendApplication.java
@@ -1,13 +1,18 @@
 package cloud.computer.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring版本号：" + SpringVersion.getVersion());
+		System.out.println("SpringBoot版本号：" + SpringBootVersion.getVersion());
+		System.out.println();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
