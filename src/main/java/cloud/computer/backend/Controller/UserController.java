d424923a8e2465d4a1680792c77c863a0d8219b6
等陌上花开 <zhzhn2002@gmail.com>
2024-01-05 11:00:07 +0800

    完善日志记录功能

--- src/main/java/cloud/computer/backend/Controller/UserController.java
+++ src/main/java/cloud/computer/backend/Controller/UserController.java
@@ -6,6 +6,8 @@ import cloud.computer.backend.Service.TokenService;
 import com.alibaba.fastjson2.JSONObject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +15,7 @@ public class UserController {
 
     private final AuthenticationService authenticationService;
     private final TokenService tokenService;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     public UserController(AuthenticationService authenticationService, TokenService tokenService) {
         this.authenticationService = authenticationService;
@@ -21,9 +24,9 @@ public class UserController {
 
     @PostMapping(value = "/login")
     public JSONObject login(@RequestBody JSONObject param){
-
         String username = param.getString("username");
         String password = param.getString("password");
+        logger.info("收到用户 " + username + " 的登录请求" );
         JSONObject result = new JSONObject();
         AuthenticationResult authenticate = this.authenticationService.authenticate(username, password);
         result.put("code", authenticate.getResult() ? 1 : 0);
@@ -32,11 +35,18 @@ public class UserController {
         }
         if (authenticate.getReason() != null) {
             switch (authenticate.getReason()){
-                case USER_NOT_FOUND -> result.put("message", "用户" + username + "不存在！");
-                case WRONG_PASSWORD -> result.put("message", "密码错误！");
+                case USER_NOT_FOUND -> {
+                    result.put("message", "用户" + username + "不存在！");
+                    logger.info("用户 " + username + " 找不到，拒绝登录");
+                }
+                case WRONG_PASSWORD -> {
+                    result.put("message", "密码错误！");
+                    logger.info("用户 " + username + " 密码错误，拒绝登录");
+                }
             }
         }else {
             result.put("message", "登录成功！");
+            logger.info("用户 " + username + " 的登录请求合规，准许登录");
         }
         return result;
     }
@@ -46,13 +56,16 @@ public class UserController {
     public JSONObject register(@RequestBody JSONObject param){
         String username = param.getString("username");
         String password = param.getString("password");
+        logger.info("收到注册请求，用户名：" + username);
         JSONObject result = new JSONObject();
         AuthenticationResult authenticate = this.authenticationService.addUser(username, password);
         result.put("code", authenticate.getResult() ? 1 : 0);
         if (authenticate.getReason() != null){
             result.put("message", "用户" + username + "已存在！");
         }
+        logger.info("注册请求处理结果：" + (authenticate.getResult() ? "成功注册": "注册失败，原因：" + authenticate.getReason().toString()));
         return result;
+
 
     }
 }
