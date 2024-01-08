e7365c9d978637674d6a3bf7a222ff0bbb015b75
等陌上花开 <zhzhn2002@gmail.com>
2024-01-08 17:33:19 +0800

    优化底层连接机制

--- src/main/java/cloud/computer/backend/DataAccess/OpenStackDataAccess.java
+++ src/main/java/cloud/computer/backend/DataAccess/OpenStackDataAccess.java
@@ -4,9 +4,14 @@ import cloud.computer.backend.Entity.OpenStackConnector;
 import cloud.computer.backend.Entity.Server;
 import org.openstack4j.api.Builders;
 import org.openstack4j.model.compute.Action;
+import org.openstack4j.model.compute.Flavor;
 import org.openstack4j.model.compute.RebootType;
 import org.openstack4j.model.compute.ServerCreate;
+import org.openstack4j.model.compute.builder.BlockDeviceMappingBuilder;
+import org.openstack4j.model.image.v2.Image;
+import org.openstack4j.model.storage.block.Volume;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.util.LinkedList;
@@ -16,19 +21,21 @@ import java.util.Objects;
 
 @Repository
 //@Scope("prototype")
-public class OpenStackKeystoneDataAccess {
+public class OpenStackDataAccess {
 
     private final OpenStackConnector connector;
     @Value("${openstack.network_id}")
     private String network_id;
     @Value("${openstack.LAN_network_id}")
     private String LAN_network_id;
+    @Value("${openstack.create_instance_timeout}")
+    private int create_instance_timeout;
 
 
     private final ServerDataAccess serverDataAccess;
 
-    public OpenStackKeystoneDataAccess(ServerDataAccess serverDataAccess,
-                                       OpenStackConnector connector) {
+    public OpenStackDataAccess(ServerDataAccess serverDataAccess,
+                               OpenStackConnector connector) {
         this.serverDataAccess = serverDataAccess;
         this.connector = connector;
     }
@@ -62,13 +69,41 @@ public class OpenStackKeystoneDataAccess {
      * 请求OpenStack创建云桌面实例
      * @param server 云桌面实例（需要设置名称、实例类型、镜像ID）
      */
-    public Server createServer(Server server,
-                               String password) {
+    @Async("desktop_creating")
+    public void createServer(Server server,
+                               String password,
+                               int owner_id) {
 //        connector.getClient() = OSFactory.clientFromToken(token);
+
+        Volume volume = connector.getClient().blockStorage().volumes().create(
+                Builders.volume()
+                        .imageRef(server.getImageId())
+                        .size(this.connector.getClient().compute().flavors().get(server.getFlavorId()).getDisk()) //大小
+                        .bootable(true)
+                        .build()
+        );
+
+        for (int i = 0; i < create_instance_timeout; i++) {
+            if(connector.getClient().blockStorage().volumes().get(volume.getId()).getStatus().name().equals("AVAILABLE")){
+                break;
+            }else {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {
+
+                }
+            }
+        }
+
+        BlockDeviceMappingBuilder blockDeviceMappingBuilder = Builders.blockDeviceMapping()
+                .uuid(volume.getId())
+                .deviceName("/dev/vda")
+                .bootIndex(0);
+
         ServerCreate sc = Builders.server()
                 .name(server.getName())
                 .flavor(server.getFlavorId())
-                .image(server.getImageId())
+                .blockDevice(blockDeviceMappingBuilder.build())
                 .addMetadataItem("admin_pass", password)
                 .networks(List.of(network_id))
                 .build();
@@ -81,8 +116,14 @@ public class OpenStackKeystoneDataAccess {
         server.setId(openstack_server.getId());
         // 将VmState设定为状态
         server.setStatus(openstack_server.getVmState());
-        return server;
+        server.setOwnerId(owner_id);
+        server.setCpuUsage(0L);
+        server.setImageName(connector.getClient().imagesV2().get(server.getImageId()).getName());
+        server.setRAM(connector.getClient().compute().flavors().get(server.getFlavorId()).getRam());
+        server.setvCPU(connector.getClient().compute().flavors().get(server.getFlavorId()).getVcpus());
+        this.serverDataAccess.addServer(server);
     }
+
 
     public void powerOffServer(String id){
         connector.getClient().compute().servers().action(id, Action.STOP);
@@ -111,6 +152,28 @@ public class OpenStackKeystoneDataAccess {
      */
     public String getPowerStatus(String id){
         return connector.getClient().compute().servers().get(id).getVmState();
+    }
+
+//    @Async
+//    public FutureTask<Server> create(Server server, String password){
+//
+//        return null;
+//    }
+
+    public Flavor getFlavor(String id){
+        return this.connector.getClient().compute().flavors().get(id);
+    }
+
+    public Map<String, ? extends Number> getInfo(String id){
+        return this.connector.getClient().compute().servers().diagnostics(id);
+    }
+
+    public List<? extends Flavor> getFlavors(){
+        return this.connector.getClient().compute().flavors().list();
+    }
+
+    public List<? extends Image> getImages(){
+        return this.connector.getClient().imagesV2().list();
     }
 
 
--- src/test/java/cloud/computer/backend/DataAccessTest/OpenStackDataAccessTest.java
+++ src/test/java/cloud/computer/backend/DataAccessTest/OpenStackDataAccessTest.java
@@ -1,6 +1,6 @@
 package cloud.computer.backend.DataAccessTest;
 
-import cloud.computer.backend.DataAccess.OpenStackKeystoneDataAccess;
+import cloud.computer.backend.DataAccess.OpenStackDataAccess;
 import cloud.computer.backend.Entity.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class OpenStackDataAccessTest {
-    private OpenStackKeystoneDataAccess openStackKeystoneDataAccess;
+    private OpenStackDataAccess openStackDataAccess;
 
     @Autowired
-    public void setOpenStackKeystoneDataAccess(OpenStackKeystoneDataAccess openStackKeystoneDataAccess) {
-        this.openStackKeystoneDataAccess = openStackKeystoneDataAccess;
+    public void setOpenStackKeystoneDataAccess(OpenStackDataAccess openStackDataAccess) {
+        this.openStackDataAccess = openStackDataAccess;
     }
 
     @Test
@@ -22,6 +22,6 @@ public class OpenStackDataAccessTest {
         server.setImageId("75a32e66-6f1d-439c-8d5d-1961b1e36471");
         server.setFlavorId("3");
         server.setName("测试服务器");
-        this.openStackKeystoneDataAccess.createServer(server, "123456");
+//        this.openStackKeystoneDataAccess.createServer(server, "123456");
     }
 }
